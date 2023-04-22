cdce8c8485db95e07f5c8827a95842849efa2502
abbosbey2002 <abbosqudratov@gmail.com>
2023-04-22 22:54:24 +0500

    crud product finished

--- index.php
+++ index.php
@@ -1,23 +1,98 @@
 <?php
 
-echo 'php';
-require_once "config/bootstrap.php"
+
+require_once "config/bootstrap.php";
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 
 
+// get all products
+if($_SERVER['REQUEST_METHOD'] === "GET" && !isset($_GET['id'])){
+  $products=Post::getAll();
+  print_r($products);
+  echo json_encode($products->title);
+  echo '</br> hi this mistake </br>';
+}
 
-?>
+// get one products
+if($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['id'])){
+  $product_id=$_GET['id'];
+  $product=Post::getOne($product_id)[0];
+  echo json_encode($product);
+}
+
+// Post products
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+  $datajson = file_get_contents("php://input", true); 
+  $data = json_decode($datajson); 
+  
+  // access the values of the properties
+  $title = $data->title;
+  $description = $data->description;
+  $catagory = $data->catagory;
+  $rate = $data->rate;
+  $price = $data->price;
+  $image = $data->image;
+  
+
+   $result=Post::postProduct($title,  $description, $catagory, $rate, $price, $image);
+   if($result){
+    echo "successfull add product";
+   }else{
+    echo 'something went wrong';
+   }
+  }
+
+  // Delete product
+
+  if($_SERVER['REQUEST_METHOD']==="DELETE"){
+    $product_id=$_GET['id'];
+    $result=Post::removeProduct($product_id, 'products');
+    if($result===1){
+      echo "successfull";
+     }else{
+      echo 'no products or error';
+     }
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>run</h1>
-</body>
-</html>
+
+    }
+
+
+    //  update product
+
+    if($_SERVER['REQUEST_METHOD']==="PUT"){
+      echo 'hello world';
+
+      $datajson = file_get_contents("php://input", true); 
+  $data = json_decode($datajson); 
+  var_dump($data);
+  // access the values of the properties
+  $id=$data->id;
+  $title = $data->title;
+  $description = $data->description;
+  $catagory = $data->catagory;
+  $rate = $data->rate;
+  $price = $data->price;
+  $image = $data->image;
+
+  $result=Post::update($id, $title,  $description, $catagory, $rate, $price, $image);
+
+
+  if($result) {
+    echo json_encode(
+      array('message' => 'product Updated')
+    );
+  } else {
+    echo json_encode(
+      array('message' => 'product Not Updated')
+    );
+  }
+    }
+  
--- src/Post.php
+++ src/Post.php
@@ -3,4 +3,120 @@
 
 class Post{
     public static $pdo;
+    public $id;
+    public $title;
+    public $descripttion;
+    public $catagory;
+    public $rate;
+    public $price;
+    public $image;
+
+    // get all product
+    public static function getAll(){
+        $stmt = self::$pdo->prepare("SELECT * FROM products");
+        $stmt -> setFetchMode(PDO::FETCH_CLASS, 'POST');
+        $stmt->execute();
+        $posts = $stmt->fetchAll();
+        return $posts;
+        
+    }
+
+    // get one product
+    public static function getOne($id){
+        $stmt = self::$pdo->prepare("SELECT * FROM products WHERE id=?");
+        $stmt -> setFetchMode(PDO::FETCH_CLASS, 'POST');
+        $stmt->execute([$id]);
+        $post = $stmt->fetchAll();
+        return $post;
+        
+    }
+
+    // POST PRODUCT
+    public static function postProduct($title,  $description, $catagory, $rate, $price, $image){
+        $title = htmlspecialchars(strip_tags($title));
+        $description = htmlspecialchars(strip_tags($description));
+        $catagory = htmlspecialchars(strip_tags($catagory));
+        $rate = htmlspecialchars(strip_tags($rate));
+        $price = htmlspecialchars(strip_tags($price));
+        $image = htmlspecialchars(strip_tags($image));
+
+
+        $query=self::$pdo->prepare("INSERT INTO products (title, description, catagory, rate, price, image) VALUES (:title, :description, :catagory, :rate, :price, :image)");
+        $query->execute([
+            'title'=>$title,
+            'description'=>$description,
+            'catagory'=>$catagory,
+            'rate'=>$rate,  
+            'price'=>$price,
+            'image'=>$image
+
+        ]);
+        return 1;
+    }
+
+    // delete product
+    public static function removeProduct($id, $table){
+        $query = 'DELETE FROM ' . $table . ' WHERE id = :id';
+
+        // Prepare statement
+        $stmt = self::$pdo->prepare($query);
+
+        // Clean data
+        $id = htmlspecialchars(strip_tags($id));
+
+        // Bind data
+        $stmt->bindParam(':id', $id);
+
+
+  
+        // Execute query
+        if($stmt->execute()) {
+          return 1;
+        }
+
+        // Print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+  }
+
+//   update product
+
+public static function update($id, $title,  $description, $catagory, $rate, $price, $image) {
+    // Create query
+    $query = 'UPDATE ' . 'products' . '
+                          SET title = :title, description = :description, catagory = :catagory, rate = :rate, price = :price, image = :image 
+                          WHERE id = :id';
+
+    // Prepare statement
+    $stmt = self::$pdo->prepare($query);
+
+    // Clean data
+    $title = htmlspecialchars(strip_tags($title));
+    $description = htmlspecialchars(strip_tags($description));
+    $catagory = htmlspecialchars(strip_tags($catagory));
+    $rate = htmlspecialchars(strip_tags($rate));
+    $price= htmlspecialchars(strip_tags($price));
+    $image = htmlspecialchars(strip_tags($image));
+
+    // Bind data
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':catagory', $catagory);
+    $stmt->bindParam(':rate', $rate);
+    $stmt->bindParam(':price', $price);
+    $stmt->bindParam(':image', $image);
+    $stmt->bindParam(':id', $id);
+
+    // Execute query
+    if($stmt->execute()) {
+      return true;
+    }
+
+    // Print error if something goes wrong
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+}
+
 }
